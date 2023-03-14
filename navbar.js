6051c8d93adee7ef5605068972e06fc6384a3a3a
Alen.m <alenmullassery123@gmail.com>
2023-03-14 18:47:50 +0530

    Navbar animation while scroll up & down

---
+++ navbar.js
@@ -0,0 +1,26 @@
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        $("#site-header").addClass("nav-fixed");
+    } else {
+        $("#site-header").removeClass("nav-fixed");
+    }
+}); $(".navbar-toggler").on("click", function () {
+    $("header").toggleClass("active");
+});
+$(document).on("ready", function () {
+    if ($(window).width() > 991) {
+        $("header").removeClass("active");
+    }
+    $(window).on("resize", function () {
+        if ($(window).width() > 991) {
+            $("header").removeClass("active");
+        }
+    });
+});
+$(function () {
+    $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+    })
+});
