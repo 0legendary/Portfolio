e488d9c356548c6df14aa37107c4c74569fb23a0
Alen.m <alenmullassery123@gmail.com>
2023-03-27 19:38:03 +0530

    looped the banner section

--- Java script/skill.js
+++ Java script/skill.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('.typed-text', {
-        strings: ['Web Designer','Web Developer','Freelancer'],
+        strings: ['Web Designer'], // Add an additional string with whitespace
         typeSpeed: 100,
+        backSpeed: 50, // Add backSpeed to control how fast the text is erased
         showCursor: false,
+        startDelay: 0,
+        loop: true,
         onComplete: function(self) {
             $(self.el).parent().find('.cursor').remove();
         }
     });
 });
-
-
